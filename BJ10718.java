d0e2bf580fdfff72e1df9757538e399d1a8a74de
Megan Lee <74307093+meg2lee@users.noreply.github.com>
2020-11-15 19:51:16 +0900

    Add files via upload

---
+++ BJ10718.java
@@ -0,0 +1,10 @@
+package com.baekjoon;
+
+public class BJ10718 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ BJ2557.java
@@ -0,0 +1,11 @@
+package com.baekjoon;
+
+public class BJ2557 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+
+	}
+
+}
